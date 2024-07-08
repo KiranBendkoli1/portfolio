fa6a449bd2b75556b5930cd65b6a198dd50fc0cf
KiranBendkoli1 <bendkolikiran@kbtcoe.org>
2024-07-08 05:53:56 +0530

    feat: updates

--- src/components/PdfComponent/PdfComponet.tsx
+++ src/components/PdfComponent/PdfComponet.tsx
@@ -1,7 +1,9 @@
 import useDimensions from 'hooks/useDimensions'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { Document, Page } from 'react-pdf'
+import { Document, Page,pdfjs } from 'react-pdf'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 export interface PdfProps {
   name: string
@@ -10,18 +12,21 @@ export interface PdfProps {
 const PdfComponet: React.FC<PdfProps> = ({ name }) => {
   const { width: windowWidth } = useDimensions()
   console.log(windowWidth)
-    const getWidth= (windowWidth:number)=>{
-        if(windowWidth<500){
-            return windowWidth-20;
-        }else if(windowWidth>500 && windowWidth<700){
-            return windowWidth-100;
-        }else{
-            return 600;
-        }
+  const getWidth = (windowWidth: number) => {
+    if (windowWidth < 500) {
+      return windowWidth - 20
+    } else if (windowWidth > 500 && windowWidth < 700) {
+      return windowWidth - 100
+    } else {
+      return 600
     }
+  }
   return (
     <div className="flex justify-center overflow-hidden  bg-backround">
-      <Document file={name} className={'w-fit text-center rounded overflow-hidden'}>
+      <Document
+        file={name}
+        className={'w-fit text-center rounded overflow-hidden'}
+      >
         <Page
           pageNumber={1}
           renderAnnotationLayer={false}
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,6 +3,7 @@ import resume from '../assets/KiranBendkoli.pdf'
 import PdfComponet from 'components/PdfComponent/PdfComponet'
 import Button from 'components/Button'
 import TitleSeperator from 'components/Seperator/TitleSeperator'
+
 const Resume = () => {
   const url =
     'https://firebasestorage.googleapis.com/v0/b/kiranbendkoli-portfolio.appspot.com/o/KiranBendkoli.pdf?alt=media&token=d69f31c4-2056-49e8-9f04-963125ea91ab'
